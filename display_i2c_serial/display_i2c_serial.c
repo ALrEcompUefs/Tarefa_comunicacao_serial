3e260007d23b185ce8ccbcb8ef4ded9807484cf1
ALrEcompUefs <contaofcali@gmail.com>
2025-02-07 10:50:11 -0300

    Protótipos
    Inserção dos pinos da GPIO utilizados e protótipos de funções da led RGB e botões

--- display_i2c_serial/display_i2c_serial.c
+++ display_i2c_serial/display_i2c_serial.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/bootrom.h"
 
+// Pinos dos perifericos
 
+const uint8_t LED_R=13, LED_B=12, LED_G=11;
+const uint8_t BOTAO_A=5;BOTAO_B=6;
+
+// protótipos de funções
+void inicializar_leds();
+void inicializar_botoes();
+void set_rgb(char cor);
+static void gpio_irq_handler(uint gpio, uint32_t events);
 
 int main()
 {
